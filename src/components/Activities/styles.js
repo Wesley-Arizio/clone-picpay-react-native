506d3671a954b23e87655ba1f09f7eb9e207b7cb
wesley_arizio <wesleyarizio26@gmail.com>
2021-05-22 16:22:26 -0300

    improving activity border bottom width

--- src/components/Activities/styles.js
+++ src/components/Activities/styles.js
@@ -32,7 +32,7 @@ export const ActivityContainer = styled.View`
 `;
 
 export const ActivityBorderBottom = styled.View`
-  width: 50%;
+  width: 100%;
   height: 2px;
   margin-top: 5px;
   background: #10c86e;
